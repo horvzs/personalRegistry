8d8743a0fb2bd79dc5ac03206a92130e39e46857
hzsolt <horvath.zsolt90@yahoo.com>
2022-10-31 22:42:53 +0100

    #9 feat: create person remover

--- src/main/resources/sql/02.insert_base_data.sql
+++ src/main/resources/sql/02.insert_base_data.sql
@@ -1,6 +1,14 @@
-insert into person (name)
-values ('Jon Doe');
-insert into dbo.address (address_type, city, street, zip_code, person_id)
-values ('PERMANENT', 'Budapest', 'Jon Doe street 20/A', '1000', '1');
-insert into contact (email, phone, address_id)
-values ('jonD@email.com', '06301112222', '1');
+DECLARE @PersonId int
+INSERT INTO personalRegistry.dbo.person (name)
+VALUES ('Jon Doe');
+
+SET @PersonId = SCOPE_IDENTITY()
+
+INSERT INTO personalRegistry.dbo.address (address_type, city, street, zip_code, person_id)
+VALUES ('PERMANENT', 'Budapest', 'Jon Doe street 20/A', '1000', @PersonId);
+
+INSERT INTO personalRegistry.dbo.address (address_type, city, street, zip_code, person_id)
+VALUES ('TEMPORARY', 'Dabas', 'Dabasi street 21', '2050', @PersonId);
+
+INSERT INTO personalRegistry.dbo.contact (email, phone, address_id)
+VALUES ('jonD@email.com', '06301112222', @PersonId);
