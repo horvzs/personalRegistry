a550c09929760ddc7cb17ec5783d3ff893ce10bd
Zsolt Horváth <horvath.zsolt0329@gmail.com>
2023-01-04 07:50:02 +0000

    docker build

--- .space.kts
+++ .space.kts
@@ -4,33 +4,30 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Build and run tests") {
-   gradlew("amazoncorretto:17-alpine", "build")
-}
-
 job("Build and push Docker") {
-    host("Build artifacts and a Docker image") {
-        // generate artifacts required for the image
-        shellScript {
-            content = """
-                ./generateArtifacts.sh
-            """
-        }
-
+    host("Build and push a Docker image") {
         dockerBuildPush {
-            // Docker context, by default, project root
+            // by default, the step runs not only 'docker build' but also 'docker push'
+            // to disable pushing, add the following line:
+            // push = false
+
+            // path to Docker context (by default, context is working dir)
             context = "docker"
-            // path to Dockerfile relative to project root
+            // path to Dockerfile relative to the project root
             // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
             file = "docker/config/Dockerfile"
-            labels["vendor"] = "mycompany"
-            args["HTTP_PROXY"] = "http://10.20.30.1:123"
-
-            val spaceRepo = "horvathzsolt.registry.jetbrains.space/p/containers/containers/"
-            // image tags for 'docker push'
+            // build-time variables
+            args["HTTP_PROXY"] = "http://10.20.30.2:1234"
+            // image labels
+            labels["vendor"] = "horvathzsolt"
+            // to add a raw list of additional build arguments, use
+            // extraArgsForBuildCommand = listOf("...")
+            // to add a raw list of additional push arguments, use
+            // extraArgsForPushCommand = listOf("...")
+            // image tags
             tags {
-                +"$spaceRepo:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
-                +"$spaceRepo:latest"
+                // use current job run number as a tag - '0.0.run_number'
+                +"horvathzsolt.registry.jetbrains.space/p/containers/containers/personalregistry:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
             }
         }
     }
