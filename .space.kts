0392319bb38d0eb486ffc284abaa9fc33c449e21
horvzs <horvath.zsolt90@yahoo.com>
2023-01-04 13:32:58 +0100

    fix: space.kts

--- .space.kts
+++ .space.kts
@@ -1,18 +1,27 @@
-job("Build and push Docker") {
+job("Build, test and create docker image") {
+    container("amazoncorretto:17-alpine", "build") {
+        shellScript {
+            content = """
+            echo Build and run tests...
+            ./gradlew clean build
+            echo Copy build dir to share path
+            cp -rv build/libs ${"$"}"JB_SPACE_FILE_SHARE_PATH"/share
+        """
+        }
+    }
     // both 'host.shellScript' and 'host.dockerBuildPush' run on the same host
     host("Build artifacts and a Docker image") {
         // Gradle build creates artifacts in ./build
-        shellScript {
-            content = """
-                ./gradlew build
-            """
-        }
-
         dockerBuildPush {
             // Note that if Dockerfile is in the project root, we don't specify its path.
             // We also imply that Dockerfile takes artifacts from ./build and puts them to image
             // e.g. with 'ADD /build/app.jar /root/home/app.jar'
-
+            this@host.shellScript {
+                content = """
+                    echo Copy build dir from share path
+                    cp -r ${"$"}"JB_SPACE_FILE_SHARE_PATH"/share
+                """.trimIndent()
+            }
             val spaceRepo = "horvathzsolt.registry.jetbrains.space/p/containers/containers/personalregistry"
             // image tags for 'docker push'
             tags {
