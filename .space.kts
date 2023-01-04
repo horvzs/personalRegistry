92f37b7f8ada82162530717e8d8d6b2178b88f81
horvzs <horvath.zsolt90@yahoo.com>
2023-01-04 13:00:10 +0100

    fix: space.kts

--- .space.kts
+++ .space.kts
@@ -1,16 +1,37 @@
-job("Build, test and publish Docker") {
+job("Build and test") {
+    container("amazoncorretto:17-alpine") {
+        shellScript {
+            content = """
+            	echo Build and run Tests...
+                ./gradlew clean build
+                docker volume create --name lib
+                echo Copy build dir...
+            	cp -rv build/libs -v lib
+            """
+        }
+    }
+}
+
+job("Build and publish Docker") {
     host("Build and push a Docker image") {
         dockerBuildPush {
-            this@job.gradlew("amazoncorretto:17-alpine", "build")
             // by default, the step runs not only 'docker build' but also 'docker push'
             // to disable pushing, add the following line:
             // push = false
 
             // path to Docker context (by default, context is working dir)
             // context = "docker"
+            this@host.shellScript {
+                content = """
+                    echo Copy build dir...
+                    cp -r -v lib docker
+                    pwd
+                """
+            }
+
             // path to Dockerfile relative to the project root
             // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
-            file = "./Dockerfile"
+            file = "docker/Dockerfile"
             // build-time variables
             args["HTTP_PROXY"] = "http://10.20.30.2:1234"
             // image labels
