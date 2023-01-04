508416a8ef0abdab87436ebe4a8ac16f505bd303
Zsolt Horváth <horvath.zsolt0329@gmail.com>
2023-01-04 08:41:33 +0000

    fix

--- .space.kts
+++ .space.kts
@@ -16,15 +16,14 @@ job ("Build, tests, and publish Docker") {
         }
     }
     
-    beforeBuildScript {
-        content = """
+    host("Build and push a Docker image") {
+        dockerBuildPush {
+            shellScript {
+                content = """
             	echo Copy files from previous step
             	cp -r /mnt/space/share docker
             """
-    }
-
-    host("Build and push a Docker image") {
-        dockerBuildPush {
+            }
             // by default, the step runs not only 'docker build' but also 'docker push'
             // to disable pushing, add the following line:
             // push = false
