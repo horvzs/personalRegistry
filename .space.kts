04324bda59c462b7cac694097668d98a035d8f9c
horvzs <horvath.zsolt90@yahoo.com>
2023-01-04 16:14:06 +0100

    fix: space.kts

--- .space.kts
+++ .space.kts
@@ -3,21 +3,22 @@ job("Build, test and create docker image") {
     host("Build artifacts and a Docker image") {
         shellScript {
             content = """
+                chmod u+r+x ./generateArtifacts.sh
                 ./generateArtifacts.sh
             """
         }
-            // Gradle build creates artifacts in ./build
-            dockerBuildPush {
+        // Gradle build creates artifacts in ./build
+        dockerBuildPush {
 
-                // Note that if Dockerfile is in the project root, we don't specify its path.
-                // We also imply that Dockerfile takes artifacts from ./build and puts them to image
-                // e.g. with 'ADD /build/app.jar /root/home/app.jar'
-                val spaceRepo = "horvathzsolt.registry.jetbrains.space/p/containers/containers/personalregistry"
-                // image tags for 'docker push'
-                tags {
-                    +"$spaceRepo:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
-                    +"$spaceRepo:latest"
-                }
+            // Note that if Dockerfile is in the project root, we don't specify its path.
+            // We also imply that Dockerfile takes artifacts from ./build and puts them to image
+            // e.g. with 'ADD /build/app.jar /root/home/app.jar'
+            val spaceRepo = "horvathzsolt.registry.jetbrains.space/p/containers/containers/personalregistry"
+            // image tags for 'docker push'
+            tags {
+                +"$spaceRepo:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+                +"$spaceRepo:latest"
             }
         }
     }
+}
